fec8b1284bb10cef884a5bb9b627befede82382b
sammer119 <sammer119@users.noreply.github.com>
2020-06-07 12:12:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MemeGenerator from './MemeGenerator'
+import Header from './Header'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <MemeGenerator />
+        </>
+    )
+}
+
+export default App
